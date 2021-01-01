3a984bdfb3e77572f59a010f841d000cb7a9f636
Andrew Bui <andrew.bui.au@gmail.com>
2021-01-01 16:16:31 +1100

    finalised application logic

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
 const taskInputButton = document.querySelector('.taskInputButton');
 const mainSectionContainer = document.querySelector('.mainSectionContainer');
 
-let i = 0;
+// Preload Mechanism
+const preLoad = function () {
 
-taskInputButton.addEventListener('click', function (event) {
+    for (let i = 0; i < localStorage.length; i++) {
+        const taskOutput = document.createElement('div');
+        const taskNotesOutput = document.createElement('p');
+        taskNotesOutput.textContent = localStorage.key(i);
+        taskOutput.classList.add('taskOutput');
+        taskOutput.appendChild(taskNotesOutput);
+        const taskOutputControls = document.createElement('button');
+        taskOutputControls.textContent = "Close Task"
+        mainSectionContainer.appendChild(taskOutput).appendChild(taskOutputControls);
+    }
+}
+
+// Preload Mechanism Call
+preLoad();
+
+
+
+// Create Task Output Mechanisim
+const createTask = function (event) {
     event.preventDefault();
-    console.log(taskInput.value);
-
-    i += 1;
 
     // Task Output Container
     const taskOutput = document.createElement('div');
-    taskOutput.textContent = i;
+    const taskNotesOutput = document.createElement('p');
+    
+    localStorage.setItem(taskInput.value, "note");
+
+    taskNotesOutput.textContent = taskInput.value;
+
     taskOutput.classList.add('taskOutput');
+    taskOutput.appendChild(taskNotesOutput);
 
     const taskOutputControls = document.createElement('button');
     taskOutputControls.textContent = "Close Task"
 
     mainSectionContainer.appendChild(taskOutput).appendChild(taskOutputControls);
+}
 
-});
+// Create Task Event Listener
+taskInputButton.addEventListener('click', createTask);
 
+
+
+// Remove Task
 mainSectionContainer.addEventListener('click', function (event) {
     event.preventDefault();
+    console.log(event.target.previousSibling);
+
+
     if (event.target.matches('button')) {
+        for (let i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i).toString() === event.target.previousSibling.textContent) {
+                localStorage.removeItem(localStorage.key(i));
+            }
+        }
         event.target.parentElement.remove();
     }
 });
